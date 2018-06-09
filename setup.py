4726f0fefb0404423e8655b2a8da3816be00e608
Eugene Kachanouski <cheryboom100@gmail.com>
2018-06-09 10:33:21 +0300

    Setup updated.

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='adastra',
     version='0.0.1',
     py_modules=['app', 'comand_line_interface'],
+    packages=find_packages(),
     entry_points='''
     [console_scripts]
     adastra=comand_line_interface.main:main
